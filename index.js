cdf0cf258911f085e26d21f5c6ed13eae76c48f3
Josh Purvis <jpurvis@kwantera.com>
2016-01-10 20:36:35 -0500

    Add interface option to override wildcard interfaces; fix some typos

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ var nip = module.exports = {
     executable: 'iptables'
 };
 
-nip._set_policies = function policies() {
-    self = this;
+nip._setPolicies = function policies() {
+    var self = this;
 
     // allow all outgoing traffic on any interface (+ wildcard), force to top of table (-I)
     self.chain.push(['-I OUTPUT -o + -d 0.0.0.0/0 -j ACCEPT']);
@@ -36,17 +36,19 @@ nip.allow = function allow(options) {
     var options = _.defaults(options, {
         'protocol': 'tcp',
         'port': null,
-        'cidr_blocks': ['0.0.0.0/0']
+        'cidr_blocks': ['0.0.0.0/0'],
+        'interface': '+' // + is wildcard, optionally restrict to a specific interface (ex: eth0)
     });
 
     if (options.cidr_blocks.constructor === String)
-        options.cidr_blocks = [options.cidr_block];
+        options.cidr_blocks = [options.cidr_blocks];
 
     _.each(options.cidr_blocks, function(cidr_block) {
         
         var inRule = [
             '-A INPUT',
             ['--protocol', options.protocol].join(" "),
+            ['--in-interface', options.interface].join(" "),
             ['--dport', options.port].join(" "),
             ['--source', cidr_block].join(" "),
             '--match state',
@@ -57,6 +59,7 @@ nip.allow = function allow(options) {
         var outRule = [
             '-A OUTPUT',
             ['--protocol', options.protocol].join(" "),
+            ['--out-interface', options.interface].join(" "),
             ['--sport', options.port].join(" "),
             ['--destination', cidr_block].join(" "),
             '--match state',
@@ -75,7 +78,7 @@ nip.allow = function allow(options) {
 nip.print = function print() {
     var self = this;
 
-    self._set_policies();
+    self._setPolicies();
     _.each(self.chain, function (link){
         console.log([self.executable, link.join(" ")].join(" "));
     });
@@ -85,9 +88,9 @@ nip.apply = function apply(cb) {
     var self = this;
     cb = cb || function() {};
 
-    self._set_policies();
+    self._setPolicies();
     async.eachSeries(self.chain, function (link, eachCallback){
-    var command = [self.executable, link.join(" ")].join(" ");
+        var command = [self.executable, link.join(" ")].join(" ");
         child_process.exec(command, function(err) {
             eachCallback(err); 
         });
